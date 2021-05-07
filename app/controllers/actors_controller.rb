735686b95b3801f10395d2c5e61897eb65dc28fa
ashley <agahagan1@gmail.com>
2021-05-07 18:17:09 -0400

    adds parameter routes

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,6 +1,14 @@
 class ActorsController < ApplicationController
+  def all_actors
+    actors = Actor.all
+    render json: actors.as_json
+  end
+  
   def one_actor
-    actor = Actor.find_by(id: 1)
+    id = params[:id]
+    actor = Actor.find(id)
     render json: actor.as_json
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/one_actor", controller: "actors", action: "one_actor"
+  get "/all_actors", controller: "actors", action: "all_actors"
+
   get "/one_movie", controller: "movies", action: "one_movie"
+
   get "/all_movies", controller: "movies", action: "all_movies"
+
+  get "/one_actor", controller: "actors", action: "one_actor"
+
+  get "/one_actor/:id", controller: "actors", action: "one_actor"
+
+  post "/one_actor_post", controller: "actors", action: "one_actor"
+
+
+
+
+
 end
